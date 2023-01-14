29053c35413f53038e9f68bfb587c61bb76d4110
mubeen <mubeenmalik>
2023-01-14 17:44:52 +0000

    first

--- src/Number.js
+++ src/Number.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Number(props) {
+ const[num, setnum]= useState(0);
   return (
-    <div>
+    <>
       {props.title}
-    </div>
+      <div>
+        {num}
+        </div>
+        <div>
+      <button className='outline outline-2'onClick={()=>setnum(num+1)}>increment</button>
+      </div>
+      <button className='outline outline-2'onClick={()=>setnum(num-1)}>decrement</button>
+    </>
   )
 }
 
